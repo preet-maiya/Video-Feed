39068150de3a31d1fe28bf3463d8da27cba56679
preet <mail.maiyapreetham@gmail.com>
2018-09-22 21:09:22 +0530

    Multiple packets with no acks

--- client.py
+++ client.py
@@ -9,12 +9,14 @@ from helpers import ReceiveVideo
 if(len(sys.argv) != 3):
     print("Usage : {} hostname port".format(sys.argv[0]))
     print("e.g.   {} 192.168.0.39 1080".format(sys.argv[0]))
-    sys.exit(-1)
+    # sys.exit(-1)
 
+host = "127.0.0.1" # sys.argv[1] 
+port = "1080" # sys.argv[2]
 
-# cv2.namedWindow("Feed")
+cv2.namedWindow("Feed")
 
-receiver = ReceiveVideo(sys.argv[1], sys.argv[2])
+receiver = ReceiveVideo(host, port)
 receiver.start()
 receiver.setOperation()
 
@@ -26,9 +28,11 @@ while(True):
         cv2.imshow("Feed", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("Exiting...")
             break
 
         if cv2.waitKey(1) & 0xFF == ord('d'):
             print("Average delay: {}".format(receiver.get_delay()))
+
     except KeyboardInterrupt:
         sys.exit()
--- helpers.py
+++ helpers.py
@@ -8,58 +8,36 @@ import numpy as np
 
 
 class VideoGrabber(Thread):
-        """A threaded video grabber.
-        
-        Attributes:
-        encode_params (): 
-        cap (str): 
-        attr2 (:obj:`int`, optional): Description of `attr2`.
-        
-        """
-        def __init__(self, jpeg_quality):
-                """Constructor.
+    def __init__(self, jpeg_quality):
+            Thread.__init__(self)
+            self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
+            self.cap = cv2.VideoCapture(0)
+            self.running = True
+            self.buffer = None
+            self.lock = Lock()
 
-                Args:
-                jpeg_quality (:obj:`int`): Quality of JPEG encoding, in 0, 100.
-                
-                """
-                Thread.__init__(self)
-                self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
-                self.cap = cv2.VideoCapture(0)
-                self.running = True
-                self.buffer = None
-                self.lock = Lock()
+    def stop(self):
+            self.running = False
 
-        def stop(self):
-                self.running = False
+    def set_quality(self, jpeg_quality):
+            self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
 
-        def set_quality(self, jpeg_quality):
-                self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
-
-        def get_buffer(self):
-                """Method to access the encoded buffer.
-
-                Returns:
-                np.ndarray: the compressed image if one has been acquired. None otherwise.
-                """
-                if self.buffer is not None:
-                        self.lock.acquire()
-                        cpy = self.buffer.copy()
-                        self.lock.release()
-                        return cpy
-                
-        def run(self):
-                while self.running:
-                        success, img = self.cap.read()
-                        if not success:
-                                continue
-                        
-                        # JPEG compression
-                        # Protected by a lock
-                        # As the main thread may asks to access the buffer
-                        self.lock.acquire()
-                        result, self.buffer = cv2.imencode('.jpg', img, self.encode_param)
-                        self.lock.release()
+    def get_buffer(self):
+            if self.buffer is not None:
+                    self.lock.acquire()
+                    cpy = self.buffer.copy()
+                    self.lock.release()
+                    return cpy
+            
+    def run(self):
+            while self.running:
+                    success, img = self.cap.read()
+                    if not success:
+                            continue
+                    
+                    self.lock.acquire()
+                    result, self.buffer = cv2.imencode('.jpg', img, self.encode_param)
+                    self.lock.release()
 
 
 class SendVideo:
@@ -76,29 +54,28 @@ class SendVideo:
     def send(self, data):
         
         if self.operation == "get":
-            # header = ''
-            # len_data = len(data)
-            # remaining = len_data
+            header = ''
+            len_data = len(data)
+            remaining = len_data
+            offset = 65490
+            frag_no = 0
+            # Remaining space after timestamp and one byte to indicate if more
+            while((remaining - 65490) > 0):
+                ts = "%.5f"%time.time()
+                more = '1'
+                header = ts + more
+                frag_data = data[frag_no*offset: (frag_no+1)*offset]
+                frag_no += 1
+                frag_data = header + frag_data
+                self.sock.sendto(frag_data, self.address)
 
-            #Remaining space after timestamp and one byte to indicate if more
-            # while((remaining - 65490) > 0):
-            #     ts = "%.5f"%time.time()
-            #     more = '1'
-            #     header = ts + more
-            #     data = header + data
-            #     self.sock.sendto(data, self.address)
-            #     ack, _ = self.sock.recvfrom(10)
-            
-            if len(data) > 65507:
-                #TODO: Handle multiple packets
-                
-                print("Not supported yet!")
-                return
-
+            more = '0'
             ts = "%.5f"%time.time()
-            data = ts+data
-            print(self.address)
-            self.sock.sendto(data, self.address)
+            header = ts + more
+            frag_data = data[frag_no*offset: (frag_no+1)*offset]
+            frag_no += 1
+            frag_data = header + frag_data
+            self.sock.sendto(frag_data, self.address)
 
 
 class ReceiveVideo(Thread):
@@ -126,21 +103,17 @@ class ReceiveVideo(Thread):
 
     def run(self):
         while(self.running):
-            data, server = self.sock.recvfrom(65507)
-            ts = time.time()
-            if len(data) == 4:
-                if(data == "FAIL"):
-                    return None
+            more, data = self.revc_data()
+            while(more):
+                print("more to come...")
+                more, tmp = self.revc_data()
+                data += tmp
 
-            ts_recv = float(data[:16])
-            delay = ts - ts_recv
-            self.handle_delay(delay)
-
-            data = data[16:]
             array = np.frombuffer(data, dtype=np.dtype('uint8'))
             img = cv2.imdecode(array, 1)
 
-            #TODO: Handle multiple packets
+            #TODO: Handle multiple packets with acks
+
             self.lock.acquire()
             self.buffer = img
             self.lock.release()
@@ -158,3 +131,20 @@ class ReceiveVideo(Thread):
         self.delay_start = time.time()
         self.delay = []
         return ret_val
+
+    def revc_data(self):
+        data, server = self.sock.recvfrom(65507)
+        ts = time.time()
+        if len(data) == 4:
+            if(data == "FAIL"):
+                return None
+
+        ts_recv = float(data[:16])
+        delay = ts - ts_recv
+        self.handle_delay(delay)
+
+        more = int(data[16])
+
+        data = data[17:]
+        
+        return (more, data)
